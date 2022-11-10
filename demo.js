a362b9ebcc5179d02bcba5c78f7c20f06f022196
Sully <vulajsv@gmail.com>
2022-11-10 03:22:08 +0000

    demo playlist

--- demo.js
+++ demo.js
@@ -132,6 +132,27 @@ document.addEventListener('DOMContentLoaded', () => {
                     previewThumbnails: {
                         image: 'https://img-vh.doodcdn.co/slides/b152cb0c0e39e020.jpg',
                     }
+                }, {
+                    type: 'video',
+                    title: 'Thor: Ragnarok',
+                    length: 183,
+                    code: 'ragnarok',
+                    poster: 'https://carousel-slider.uiinitiative.com/images/thor-ragnarok.jpg',
+                    sources: [
+                        {
+                            src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4',
+                            type: 'video/mp4',
+                            size: '720'
+                        }, {
+                            src: 'https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4',
+                            type: 'video/mp4',
+                            size: '1080'
+                        }
+                    ],
+                    tracks: [],
+                    previewThumbnails: {
+                        image: 'https://img-vh.doodcdn.co/slides/b152cb0c0e39e020.jpg',
+                    }
                 }
             ]
         }
